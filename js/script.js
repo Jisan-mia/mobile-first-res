68d56ef2d8c0feb217d301639340b1ebd6d43384
Jisan-mia <jisanmia47@gmail.com>
2021-03-25 14:02:07 +0600

    designing the page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const navLists = document.querySelector(".nav-lists");
+
+hamburger.addEventListener("click", function () {
+	navLists.classList.toggle("show");
+});
